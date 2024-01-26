044232952ac5d9f78ad14806647c68bc64879d05
Angel Lopez <arturolopez15@hotmail.com>
2024-01-26 01:53:30 -0500

    Fin seccion 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
 
-
+import { AuthModule } from './auth/auth.module';
+import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NotPageFoundComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    FooterComponent,
-    ProgressComponent,
-    Grafica1Component,
-    DashboardComponent,
-    PagesComponent,
 
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, PagesModule,AuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
